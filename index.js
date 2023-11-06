9e5b9c42e89f384c717eec32394f173435fc0660
abdulhamid4663 <abdulhamid4663@gmail.com>
2023-11-07 01:13:59 +0600

    added delete a single food by id functionality

--- index.js
+++ index.js
@@ -108,6 +108,14 @@ async function run() {
             res.send(result);
         });
 
+        // DELETE a single food by id
+        app.delete("/foods/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await foodCollection.deleteOne(query);
+            res.send(result);
+        });
+
 
 
 
